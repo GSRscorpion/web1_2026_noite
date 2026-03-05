1c261a630627c26ae0c37c2ef1341912f05945a5
GSRscorpion <gabrielrochascorpion@gmail.com>
2026-03-04 21:41:52 -0300

    Add files via upload

--- webi_noite/aula8/js/script.js
+++ webi_noite/aula8/js/script.js
@@ -1,5 +1,4 @@
-//Mapeamento dos componetes HTML
-
+// Mapeamento dos componentes HTML
 const numero = document.getElementById("numero")
 const btnAdd = document.getElementById("btn-add")
 const txtContar = document.getElementById("txt-contar")
@@ -14,58 +13,109 @@ const menor = document.getElementById("menor")
 const btnFinalizar = document.getElementById("btn-finalizar")
 const btnReiniciar = document.getElementById("btn-reiniciar")
 
-//lógica
+// Lista de números
+var listaNumero = []
 
-var listaNumero = Array()
-
-btnFinalizar.setAttribute("disable", null)
+btnFinalizar.setAttribute("disabled", null)
 btnFinalizar.style.cursor = "not-allowed"
 
-
-btnAdd.addEventListener("click", ()=>{
-    if(numero.value == "")
-        {
-            alert("Insira um número!")
-        }
-        else
-        {
-            listaNumero.push( Number (numero.value) )
-            txtContar.innerHTML = "Restam " + (10 - listaNumero.length) + " Números" 
-            
-            const newOption = document.createElement("option")
-            newOption.id =numero.value + "-id"
-            newOption.text =numero.value
-            inseridos.appendChild(newOption)
-
-            inseridos.size = listaNumero.length + 1
-
-            habilitarBotao()
-            
-            console.log(listaNumero)
-        }
+// Pressionar Enter adiciona o número
+numero.addEventListener("keypress", (elm)=>{
+    if(elm.key == "Enter"){
+        btnAdd.click()
+    }
 })
 
+// Adicionar número
+btnAdd.addEventListener("click", ()=>{
+    if(numero.value == ""){
+        alert("Insira um número!")
+    } else if(testanumero() !== undefined){
+        alert("Número " + numero.value + " já inserido!")
+    } else {
+        listaNumero.push(Number(numero.value))
+        txtContar.innerHTML = "Restam " + (10 - listaNumero.length) + " Números" 
+        
+        const newOption = document.createElement("option")
+        newOption.id = numero.value + "-id"
+        newOption.text = numero.value
+        inseridos.appendChild(newOption)
+
+        // Remove opção inicial se houver
+        const initialOpt = document.getElementById("opt1")
+        if(initialOpt) initialOpt.remove()
+
+        habilitarBotao()
+        numero.value = ""
+        numero.focus()
+        console.log(listaNumero)
+    }
+})
+
+// Finalizar: mostrar estatísticas
 btnFinalizar.addEventListener("click", ()=>{
     primeiro.innerHTML = listaNumero[0]
     ultimo.innerHTML = listaNumero.at(-1)
-    soma.innerHTML = listaNumero.reduce((x, y)=>x + y)
-    maior.innerHTML = listaNumero.reduce((x,y)=>Math.max(x,y) )
-    menor.innerHTML = listaNumero.reduce((x,y)=>Math.min(x,y) )
+    soma.innerHTML = listaNumero.reduce((x, y) => x + y, 0)
+    maior.innerHTML = Math.max(...listaNumero)
+    menor.innerHTML = Math.min(...listaNumero)
 })
 
+// Reiniciar página
 btnReiniciar.addEventListener("click", ()=>{
     window.location.reload()
 })
 
-function habilitarBotao()
-{
-    if(listaNumero.length == 10 )
-    {
-        btnFinalizar.removeAttribute("disable")
+// Habilitar botão finalizar
+function habilitarBotao(){
+    if(listaNumero.length == 10 ){
+        btnFinalizar.removeAttribute("disabled")
         btnFinalizar.style.cursor = "pointer"
-        numero.setAttribute("disable", null)
+        numero.setAttribute("disabled", null)
         numero.style.cursor = "not-allowed"
-        btnAdd.setAttribute("disable", null)
+        btnAdd.setAttribute("disabled", null)
         btnAdd.style.cursor = "not-allowed"
     }
+}
+
+// Apagar números selecionados
+btnApaga.addEventListener("click", ()=>{
+    let selecionados = []
+
+    for(let ind = 0; ind < inseridos.options.length; ind++){
+        if(inseridos.options[ind].selected){
+            selecionados.push(inseridos.options[ind].value)
+        }
+    }
+
+    selecionados.forEach((num)=>{
+        listaNumero = listaNumero.filter((mento)=> mento != Number(num))
+        const optToRemove = document.getElementById(num+"-id")
+        if(optToRemove) optToRemove.remove()
+    })
+
+    txtContar.innerHTML = "Restam " + (10 - listaNumero.length) + " Números"
+
+    if(listaNumero.length < 10){
+        btnFinalizar.setAttribute("disabled", null)
+        btnFinalizar.style.cursor = "not-allowed"
+        numero.removeAttribute("disabled")
+        numero.style.cursor = "pointer"
+        btnAdd.removeAttribute("disabled")
+        btnAdd.style.cursor = "pointer"
+    }
+
+    if(listaNumero.length == 0){
+        const optnew = document.createElement("option")
+        optnew.id = "opt1"
+        optnew.text = "Insira um Número"
+        inseridos.appendChild(optnew)
+    }
+
+    console.log(listaNumero)
+})
+
+// Testa se o número já foi inserido
+function testanumero(){
+    return listaNumero.find((num)=> numero.value == num)
 }
